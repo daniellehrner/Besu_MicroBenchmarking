230df6234d6f4293e9b9a19d42ffd571ffca5bc9
Daniel Lehrner <daniel.lehrner@consensys.net>
2024-12-13 13:34:46 +0100

    simplify benchmark, make sure no caching can happen

--- src/main/java/net/consensys/codedelegation/CodeDelegationProcessorBenchmark.java
+++ src/main/java/net/consensys/codedelegation/CodeDelegationProcessorBenchmark.java
@@ -15,15 +15,12 @@ package net.consensys.codedelegation;
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.crypto.KeyPair;
 import org.hyperledger.besu.crypto.SECP256K1;
-import org.hyperledger.besu.crypto.SignatureAlgorithm;
-import org.hyperledger.besu.crypto.SignatureAlgorithmFactory;
 import org.hyperledger.besu.datatypes.Address;
-import org.hyperledger.besu.datatypes.TransactionType;
-import org.hyperledger.besu.ethereum.core.Transaction;
+import org.hyperledger.besu.ethereum.core.CodeDelegation;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -57,80 +54,66 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @State(Scope.Benchmark)
 public class CodeDelegationProcessorBenchmark {
 
-    private SignatureAlgorithm signatureAlgorithm;
-    private static final SECP256K1 secp256k1 = new SECP256K1();
-    private Transaction transaction;
+  private static final SECP256K1 secp256k1 = new SECP256K1();
+  private List<CodeDelegation> sequentialCodeDelegations;
+  private List<CodeDelegation> parallelCodeDelegations;
 
-    @Param({"10", "100", "1000"})
-    private int delegationCount;
+  @Param({"10", "100", "1000"})
+  private int delegationCount;
 
-    @Setup
-    public void setup() {
-        signatureAlgorithm = SignatureAlgorithmFactory.getInstance();
-        assert (signatureAlgorithm.isNative());
-        // Create a transaction with code delegations
-        List<org.hyperledger.besu.datatypes.CodeDelegation> codeDelegations =
-                createCodeDelegations(delegationCount);
-        transaction = createTransactionWithDelegations(codeDelegations);
+  @Setup
+  public void setup() {
+    assert (secp256k1.isNative());
+    sequentialCodeDelegations = createCodeDelegations(delegationCount);
+    parallelCodeDelegations = createCodeDelegations(delegationCount);
+  }
+
+  private List<CodeDelegation> createCodeDelegations(
+    final int count) {
+    List<CodeDelegation> delegations = new ArrayList<>();
+
+    for (int i = 0; i < count; i++) {
+      final KeyPair keyPair = secp256k1.generateKeyPair();
+      CodeDelegation codeDelegation =
+        (CodeDelegation) CodeDelegation.builder()
+          .chainId(BigInteger.valueOf(i))
+          .address(Address.wrap(Bytes.random(20)))
+          .nonce(i)
+          .signAndBuild(keyPair);
+      delegations.add(codeDelegation);
     }
+    return delegations;
+  }
 
-    private List<org.hyperledger.besu.datatypes.CodeDelegation> createCodeDelegations(
-            final int count) {
-        List<org.hyperledger.besu.datatypes.CodeDelegation> delegations = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-            final KeyPair keyPair =
-                    SignatureAlgorithmFactory.getInstance().generateKeyPair();
-            org.hyperledger.besu.datatypes.CodeDelegation codeDelegation =
-                    org.hyperledger.besu.ethereum.core.CodeDelegation.builder()
-                            .chainId(BigInteger.valueOf(20211))
-                            .address(Address.fromHexString("0x" + String.format("%040x", i)))
-                            .nonce(i)
-                            .signAndBuild(keyPair);
-            delegations.add(codeDelegation);
-        }
-        return delegations;
+  @Benchmark
+  public void sequentialProcessing(final Blackhole bh) {
+    for (CodeDelegation codeDelegation : sequentialCodeDelegations) {
+//      System.out.println("nonce: " + codeDelegation.nonce() + ", chainId: "
+//        + codeDelegation.chainId() + ", address: " + codeDelegation.address()
+//        + ", signature: " + codeDelegation.signature());
+      final Optional<Address> authorizer = codeDelegation.authorizer();
+//      System.out.println(authorizer.get());
+      bh.consume(authorizer.get());
     }
+  }
 
-    private Transaction createTransactionWithDelegations(
-            final List<org.hyperledger.besu.datatypes.CodeDelegation> delegations) {
-        return new TransactionTestFixture().codeDelegations(delegations).type(TransactionType.DELEGATE_CODE).createTransaction(SignatureAlgorithmFactory.getInstance().generateKeyPair());
-    }
+  @Benchmark
+  public void parallelProcessing(final Blackhole bh) {
+    parallelCodeDelegations.parallelStream()
+      .forEach(
+        codeDelegation -> {
+          final Optional<Address> authorizer = codeDelegation.authorizer();
+          bh.consume(authorizer.get());
+        });
 
-    @Benchmark
-    public void sequentialProcessing(final Blackhole bh) {
-        transaction
-                .getCodeDelegationList()
-                .ifPresent(
-                        delegations -> {
-                            for (org.hyperledger.besu.datatypes.CodeDelegation codeDelegation : delegations) {
-                                final Optional<Address> authorizer = codeDelegation.authorizer();
-                                bh.consume(authorizer);
-                            }
-                        });
-    }
+  }
 
-    @Benchmark
-    public void parallelProcessing(final Blackhole bh) {
-        transaction
-                .getCodeDelegationList()
-                .ifPresent(
-                        delegations -> {
-                            delegations.parallelStream()
-                                    .forEach(
-                                            codeDelegation -> {
-                                                final Optional<Address> authorizer = codeDelegation.authorizer();
-                                                bh.consume(authorizer);
-                                            });
-                        });
-    }
+  public static void main(final String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder()
+      .include(CodeDelegationProcessorBenchmark.class.getSimpleName())
+      .addProfiler(AsyncProfiler.class)
+      .build();
 
-    public static void main(final String[] args) throws RunnerException, IOException {
-        Options opt = new OptionsBuilder()
-                .include(CodeDelegationProcessorBenchmark.class.getSimpleName())
-                .addProfiler(AsyncProfiler.class)
-                .build();
-
-        new Runner(opt).run();
-    }
+    new Runner(opt).run();
+  }
 }
